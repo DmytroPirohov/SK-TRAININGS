e3524e52fe11c69917d1ed0b978401900dcba79d
Dmytro <dimapirohov@gmail.com>
2021-04-08 19:09:08 +0300

    Add training18

--- app/js/script.js
+++ app/js/script.js
@@ -23,17 +23,101 @@
 
 // console.log(sum);
 
-let users = [];
+// let users = [];
 
-for (let i = 0; i < 3; i++) {
-  let str = prompt("Input your name", "");
-  if (str == null) {
-    console.log("cancel was pressed");
-    break;
-  } else {
-    users.push(str);
-  }
-  alert("Your name" + str + "!");
+// for (let i = 0; i < 3; i++) {
+//   let str = prompt("Input your name", "");
+//   if (str == null) {
+//     console.log("cancel was pressed");
+//     break;
+//   } else {
+//     users.push(str);
+//   }
+//   alert("Your name" + str + "!");
+// }
+
+// console.log(users);
+
+//Practika 2
+// let str = "Lorem ipsuma is simplyyy dummy loreman";
+// let arr = str.split(" ");
+// let countWords = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i].length > 6) {
+//     console.log(arr[i]);
+//   }
+// }
+
+//Praktika sortirovka po simvolam
+
+// let str = "Lorem ipsuma is simplyyy dummy loreman";
+// let arr = str.split(" ");
+// let arr = [1, 2, 10];
+// arr.sort();
+// console.log(arr);
+
+//Sravnenie
+
+// function compareNumeric(a, b) {
+//   if (a > b) return 1;
+//   if (a == b) return 0;
+//   if (a < b) return -1;
+// }
+// let arr = [1, 2, 10];
+// arr.sort(compareNumeric);
+// console.log(arr);
+
+//reverse
+// arr.reverse(compareNumeric);
+// console.log(arr);
+
+//soedinenie
+
+// let newArr = [1, 2];
+// let newArrQ = arr.concat(3, 4);
+
+// console.log(newArrQ);
+
+//Practica tr18
+
+// function randomNumber(min, max) {
+//   let rand = min + Math.random() * (max - min);
+//   return Math.round(rand);
+// }
+
+// alert(randomNumber(1, 100));
+
+// let arr = [1, 2, 3, 4];
+// let arg = promt("input your number", "0");
+// let res;
+
+// if (arg != null) {
+//   let res = arr.indexOf(+arg);
+// } else {
+//   console.log("Cancel was pressed");
+// }
+
+// if (res == -1) {
+//   console.log("Num not found");
+// } else {
+//   console.log(res);
+// }
+
+/////
+let arr = [1, 2, 3, 4];
+let arg = prompt("input your number", "0");
+
+if (arg != null) {
+  searchElem(arg);
+} else {
+  console.log("Cancel was pressed");
 }
 
-console.log(users);
+function searchElem(arg) {
+  let res = arr.indexOf(+arg);
+  if (res == -1) {
+    console.log("Num not found");
+  } else {
+    console.log(res);
+  }
+}
